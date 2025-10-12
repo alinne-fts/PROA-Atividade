291ef1cbf3d964b5d04441a6150156c674c2f8cc
line676 <alinnefernanda997@gmail.com>
2025-10-12 20:40:44 -0300

    atividade17

---
+++ Atividade17-REACTjs-PrimeiroAppCompleto/agencia_de_viagens/src/App.jsx
@@ -0,0 +1,28 @@
+
+import './App.css'
+import Header from './componentes/Header'
+import Home from './componentes/Home'
+import Footer from './componentes/Footer'
+import Escocia from './componentes/Escocia'
+import GrandCanyon from './componentes/GrandCanyon'
+import MuralhasDaChina from './componentes/MuralhasDaChina'
+import Aruba from './componentes/Aruba'
+
+function App() {
+
+  return (
+    <>
+      <main>
+        <Header />
+        <Home />
+        <Footer />
+        <Escocia />
+        <GrandCanyon />
+        <MuralhasDaChina />
+        <Aruba />
+      </main>
+    </>
+  )
+}
+
+export default App
